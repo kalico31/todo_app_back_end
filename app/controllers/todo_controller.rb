a600a95cb9223e0aadb9bb2fe6189e5f5b3bee2f
Karissa Cordova <icecreamxd9@gmail.com>
2017-10-01 16:26:06 +0000

    my message2

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,9 +8,29 @@ class TodoController<ApplicationController
         todo_id=params[:id].to_i
         @todo=Todo.find_by_id(todo_id)
         
+        def new
+        end
+        
+        def create
+         t = Todo.new
+         t.description = params['description']
+         t.pomodoro_estimate = params['pomodoro_estimate']
+         t.save
+         redirect_to "/todo/show/#{t.id}"
+        end
+        
            
            
            
     end
-    
+    def new
+       end
+       
+       def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+       end
 end
